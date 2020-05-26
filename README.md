141a7e0af3b00f3bf2b89d6a00718222eb138e29
sharshi <sharshib@gmail.com>
2020-05-26 17:44:10 -0400

    Siddur content

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # shafeh.org
+![Gatsby Publish](https://github.com/ShafehOrg/site/workflows/Gatsby%20Publish/badge.svg)
---
+++ src/pages/siddur-support.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const SiddurSupport = () => (
+  <Layout>
+    <SEO title="Siddur Support" />
+    <h1>Siddur Support</h1>
+    <p></p>
+    <Link to="/">home</Link>
+  </Layout>
+)
+
+export default SiddurSupport;
