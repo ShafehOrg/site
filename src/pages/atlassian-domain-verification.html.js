e5bd6ab7e41d369380ea3821cf971d901676b7e7
sharshi <sharshib@gmail.com>
2020-07-15 11:12:12 -0400

    atlassian

---
+++ src/pages/atlassian-domain-verification.html.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const AtlassianTXT = () => (
+  <Layout>
+    <SEO title="Domain Verification Token" />
+    <h1>Domain Verification Token</h1>
+    <p>sWNeaMVpCnW9UigZ/NXHld0NwRagnapkNtdfoa9mVN2Pa06oLoi1MdjCfPY64gj3</p>
+  </Layout>
+)
+
+export default AtlassianTXT
