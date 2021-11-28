ed66d8470fff45f352700d61f225f07a94b00cf8
Shersheial Borisute <4740525+sharshi@users.noreply.github.com>
2021-11-28 12:02:01 -0500

    add privacy page

---
+++ src/pages/privacy.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Privacy = () => (
+  <Layout>
+    <SEO title="Siddur Privacy" />
+    <h1>Privacy</h1>
+    <p>No data is collected from the siddur app.</p>
+  </Layout>
+)
+
+export default Privacy;
